8fe0bd83efdecc43cc1be4b7550db098a45f8d9c
Pallava Joshi <90088723+Pallava-Joshi@users.noreply.github.com>
2022-11-12 04:35:03 +0530

    Add files via upload

---
+++ Instagram-Clone-main/client/build/precache-manifest.b7acfd4e49ed9a9e2384bba6369e7554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912d82f187f219a435261110f3a8fdbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ed6e87c6bc71dad0ec",
+    "url": "/static/css/main.a4135bf3.chunk.css"
+  },
+  {
+    "revision": "70c9184443c804ee3210",
+    "url": "/static/js/2.05df021f.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.05df021f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95ed6e87c6bc71dad0ec",
+    "url": "/static/js/main.91c8e7d6.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
---
+++ Instagram-Clone-main/config/dev.js
@@ -0,0 +1,16 @@
+// module.exports = {
+//   MONGOURI:
+//     "mongodb+srv://pallava:7dZEYzYCctsgwDu@cluster0.coa7f.mongodb.net/Instagram_Clone?retryWrites=true&w=majority",
+//   JWT_SECRET: "instagramclone",
+//   SENDGRID_API:
+//     "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 ",
+//   EMAIL: "http:localhost:3000",
+// };
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://HbRu5CVLQQWREODn:HbRu5CVLQQWREODn@cluster0.awvgj.mongodb.net/Instagram_Clone?retryWrites=true&w=majority",
+  JWT_SECRET: "instagramclone",
+  SENDGRID_API:
+    "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",
+  EMAIL: "http:localhost:3000",
+};
